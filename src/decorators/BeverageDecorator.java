c1e2381bac323d8f6dc7e1c413e5e55465cbf049
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-02-21 00:28:41 +0100

    Added BeverageDecorator abstract class implementing IBeverage

---
+++ src/decorators/BeverageDecorator.java
@@ -0,0 +1,11 @@
+package decorators;
+
+import beverages.IBeverage;
+
+public abstract class BeverageDecorator implements IBeverage {
+    protected IBeverage beverage;
+
+    public BeverageDecorator(IBeverage beverage) {
+        this.beverage = beverage;
+    }
+}
